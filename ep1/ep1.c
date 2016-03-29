afb412b05c8db8ddeeb3daa267474532b9e78dc3
Marcelo Paiva <paiva.mna@gmail.com>
2016-03-29 01:30:50 -0300

    Le entrada

---
+++ ep1/ep1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int tabuleiro;
+
+    printf("Jogo da Velha Dancarina\n");
+    printf("Tabuleiro inicial: ");
+    scanf("%d", &tabuleiro);
+
+    printf("%d\n", tabuleiro);
+
+    return 0;
+}
+
+
